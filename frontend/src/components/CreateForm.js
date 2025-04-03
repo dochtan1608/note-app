4d3b5dd39ed4c6657fa8d4cb41de3fcf02306e3e
PaimonUwU <31623186+PTHyperdrive@users.noreply.github.com>
2025-04-03 15:08:35 +0700

    Update CreateForm.js

--- frontend/src/components/CreateForm.js
+++ frontend/src/components/CreateForm.js
@@ -1,20 +1,14 @@
+// CreateForm.jsx
 import notesStore from "../stores/notesStore";
 
-// CreateForm Component
-// Mục đích: Hiển thị form tạo note.
-// Vị trí: Cột phải trong layout 2 cột.
-// Chức năng: Cho phép người dùng nhập tiêu đề và nội dung của note, sau đó submit để tạo note mới.
 export default function CreateForm() {
   const store = notesStore();
 
-  // Nếu đang trong trạng thái cập nhật note (updateForm có _id) thì không hiển thị form tạo note.
-  if (store.updateForm._id) return <></>;
+  if (store.updateForm._id) return null;
 
   return (
     <div className="create-form-section">
-      {/* Tiêu đề của form */}
       <h2>Create Note</h2>
-      {/* Form: Gọi hàm store.createNote khi submit */}
       <form onSubmit={store.createNote}>
         <input
           className="input-field"
@@ -29,10 +23,10 @@ export default function CreateForm() {
           value={store.createForm.body}
           name="body"
           placeholder="Body"
-          rows="5"
+          rows="4"
         />
         <button className="btn" type="submit">
-          Create note
+          Create Note
         </button>
       </form>
     </div>
